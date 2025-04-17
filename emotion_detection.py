1480214b0a4ff93a9814f102f5786c5f70c28222
rachelstiansen <rachelstiansen@gmail.com>
2025-04-17 05:56:52 -0400

    Trying to figure out task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,25 @@
 import requests
+import json
 
-def emotion_detector(text_to_analye):
+def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header) 
-    return response.text
+    formatted_response = json.loads(url, json=myobj, headers=header)
+    
+    anger_score = formatted_response["documentSentiment"]["anger"]
+    disgust_score = formatted_response["documentSentiment"]["disgust"]
+    fear_score = formatted_response["documentSentiment"]["fear"]
+    joy_score = formatted_response["documentSentiment"]["joy"]
+    sadness_score = formatted_response["documentSentiment"]["sadness"]
+    dominant_emotion = response_json['emotion_scores']
+
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score,
+            'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': '<name of the dominant emotion>'}
+
+"""
+import json
+from emotion_detection import emotion_detector
+response = emotion_detector("I love this new technology")
+"""
